328a94efb1fa743f4c23caac65723902bb6ada3e
Tarissazzhr <tarissa1402@gmail.com>
2024-05-26 15:36:47 +0700

    update banyak

--- app/src/main/java/com/example/projekf/HomeActivity.kt
+++ app/src/main/java/com/example/projekf/HomeActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.projekf.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home   -> replaceFragment(HomeFragment())
+                R.id.explore -> replaceFragment(ExploreFragment())
+                R.id.cart -> replaceFragment(CartFragment())
+                R.id.offer -> replaceFragment(OfferFragment())
+                R.id.account -> replaceFragment(AccountFragment())
+
+                else -> {
+                }
+            }
+            true
         }
+    }
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
